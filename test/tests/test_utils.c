6e7de4146aa8423eba0668209f8d3b5cfd650bb8
Travis Matthews <trav.matth@gmail.com>
2019-12-21 23:48:38 -0800

    detect tests run on single file

--- test/tests/test_utils.c
+++ test/tests/test_utils.c
@@ -45,58 +45,26 @@ test_determine_file_detects_invalid(
 	return MUNIT_OK;
 }
 
-extern char* fat_file_params[];
-
-static MunitParameterEnum detect_fat_params[] = {
-  { "fat_file", fat_file_params },
-  { NULL, NULL },
-};
-
-static void
-*detect_fat_file_test_setup(
-	const MunitParameter params[], MUNIT_UNUSED void *user_data) {
-	int	*status = (int*)malloc(sizeof(int));
-	if (strcmp("test/artifacts/fat/fat_corr7", params->value))
-		*status = EXIT_SUCCESS;
-	else
-		*status = EXIT_FAILURE;
-	return (void*)status;
-}
-
-static void
-detect_fat_file_test_teardown(void *user_data) {
-  free(user_data);
-}
-
 static MunitResult
 test_determine_file_detects_fat(
-	const MunitParameter params[], void *fixture) {
+	MUNIT_UNUSED const MunitParameter params[], MUNIT_UNUSED void *fixture) {
 	t_ctx ctx;
-	int	result = *(int*)fixture;
-	char *argv[2] = { NULL, params->value };
+	char *argv[2] = { NULL, "test/artifacts/fat/fat" };
 
 	bzero(&ctx, sizeof(t_ctx));
 	munit_assert_int(get_file(2, argv, NULL, &ctx), ==, EXIT_SUCCESS);
-	munit_assert_int(determine_file(&ctx), ==, result);
-	if (result == EXIT_SUCCESS)
-		munit_assert_true(ctx.flags & IS_FAT);
+	munit_assert_int(determine_file(&ctx), ==, EXIT_SUCCESS);
+	munit_assert_true(ctx.flags & IS_FAT);
 	if (cleanup_ctx(&ctx) != EXIT_SUCCESS)
 		return MUNIT_FAIL;
 	return MUNIT_OK;
 }
 
-extern char* i386_file_params[];
-
-static MunitParameterEnum detect_i386_params[] = {
-  { "i386_file", i386_file_params },
-  { NULL, NULL },
-};
-
 static MunitResult
 test_determine_file_detects_i386(
-	const MunitParameter params[], MUNIT_UNUSED void *fixture) {
+	MUNIT_UNUSED const MunitParameter params[], MUNIT_UNUSED void *fixture) {
 	t_ctx ctx;
-	char *argv[2] = { NULL, params->value };
+	char *argv[2] = { NULL, "test/artifacts/binary/main32" };
 
 	bzero(&ctx, sizeof(t_ctx));
 	munit_assert_int(get_file(2, argv, NULL, &ctx), ==, EXIT_SUCCESS);
@@ -107,18 +75,11 @@ test_determine_file_detects_i386(
 	return MUNIT_OK;
 }
 
-extern char* x86_64_file_params[];
-
-static MunitParameterEnum detect_x86_64_params[] = {
-  { "x86_64_file", x86_64_file_params },
-  { NULL, NULL },
-};
-
 static MunitResult
 test_determine_file_detects_x86_64(
-	const MunitParameter params[], MUNIT_UNUSED void *fixture) {
+	MUNIT_UNUSED const MunitParameter params[], MUNIT_UNUSED void *fixture) {
 	t_ctx ctx;
-	char *argv[2] = { NULL, params->value };
+	char *argv[2] = { NULL, "test/artifacts/binary/a.out" };
 
 	bzero(&ctx, sizeof(t_ctx));
 	munit_assert_int(get_file(2, argv, NULL, &ctx), ==, EXIT_SUCCESS);
@@ -129,18 +90,11 @@ test_determine_file_detects_x86_64(
 	return MUNIT_OK;
 }
 
-extern char	*ar_file_params[];
-
-static MunitParameterEnum detect_ar_params[] = {
-  { "archive_file", ar_file_params },
-  { NULL, NULL },
-};
-
 static MunitResult
 test_determine_file_detects_archive(
-	const MunitParameter params[], MUNIT_UNUSED void *fixture) {
+	MUNIT_UNUSED const MunitParameter params[], MUNIT_UNUSED void *fixture) {
 	t_ctx ctx;
-	char *argv[2] = { NULL, params->value };
+	char *argv[2] = { NULL, "test/artifacts/archive/_lft.a" };
 
 	bzero(&ctx, sizeof(t_ctx));
 	munit_assert_int(get_file(2, argv, NULL, &ctx), ==, EXIT_SUCCESS);
@@ -151,18 +105,11 @@ test_determine_file_detects_archive(
 	return MUNIT_OK;
 }
 
-extern char	*ar_extended_file_params[];
-
-static MunitParameterEnum detect_ar_extended_params[] = {
-  { "archive_file", ar_extended_file_params },
-  { NULL, NULL },
-};
-
 static MunitResult
 test_determine_file_detects_extended_archive(
-	const MunitParameter params[], MUNIT_UNUSED void *fixture) {
+	MUNIT_UNUSED const MunitParameter params[], MUNIT_UNUSED void *fixture) {
 	t_ctx ctx;
-	char *argv[2] = { NULL, params->value };
+	char *argv[2] = { NULL, "test/artifacts/archive/test.a" };
 
 	bzero(&ctx, sizeof(t_ctx));
 	munit_assert_int(get_file(2, argv, NULL, &ctx), ==, EXIT_SUCCESS);
@@ -178,11 +125,11 @@ static MunitTest tests[] = {
 { "test_get_file_fails_dir", test_get_file_fails_dir, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
 { "test_get_file_opens_file", test_get_file_opens_file, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
 { "test_determine_file_detects_invalid", test_determine_file_detects_invalid, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
-{ "test_determine_file_detects_fat", test_determine_file_detects_fat, detect_fat_file_test_setup, detect_fat_file_test_teardown, MUNIT_TEST_OPTION_NONE, detect_fat_params },
-{ "test_determine_file_detects_i386", test_determine_file_detects_i386, NULL, NULL, MUNIT_TEST_OPTION_NONE, detect_i386_params },
-{ "test_determine_file_detects_x86_64", test_determine_file_detects_x86_64, NULL, NULL, MUNIT_TEST_OPTION_NONE, detect_x86_64_params },
-{ "test_determine_file_detects_archive", test_determine_file_detects_archive, NULL, NULL, MUNIT_TEST_OPTION_NONE, detect_ar_params },
-{ "test_determine_file_detects_extended_archive", test_determine_file_detects_extended_archive, NULL, NULL, MUNIT_TEST_OPTION_NONE, detect_ar_extended_params },
+{ "test_determine_file_detects_fat", test_determine_file_detects_fat, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+{ "test_determine_file_detects_i386", test_determine_file_detects_i386, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+{ "test_determine_file_detects_x86_64", test_determine_file_detects_x86_64, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+{ "test_determine_file_detects_archive", test_determine_file_detects_archive, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+{ "test_determine_file_detects_extended_archive", test_determine_file_detects_extended_archive, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
 /* Mark the end of the array with an entry where the test function is NULL */
 { NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
 };
