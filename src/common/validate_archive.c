a37d9457cc2a679ebd1673730c7bb5c78d429c31
Travis Matthews <tmatthew@e1z3r8p8.42.us.org>
2020-01-23 10:50:18 -0800

    trying to determine meaning of bytes beyond struct ar_hdr

--- src/common/validate_archive.c
+++ src/common/validate_archive.c
@@ -6,7 +6,7 @@
 /*   By: tmatthew <tmatthew@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/15 14:55:26 by tmatthew          #+#    #+#             */
-/*   Updated: 2020/01/15 15:55:17 by tmatthew         ###   ########.fr       */
+/*   Updated: 2020/01/23 10:48:01 by tmatthew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int		validate_extended_archive(char *file, t_ctx __attribute__((unused)) *ctx)
 {
-	struct ar_hdr	header __attribute__((unused));
+	struct ar_hdr	file_header __attribute__((unused));
 
-	header = *(struct ar_hdr*)(file + SARMAG);
-	return (validate_file(file + sizeof(struct ar_hdr), ctx, TRUE));
+	file_header = *(struct ar_hdr*)(file + SARMAG);
+	return (validate_file(file + SARMAG + sizeof(struct ar_hdr), ctx, TRUE));
 }
